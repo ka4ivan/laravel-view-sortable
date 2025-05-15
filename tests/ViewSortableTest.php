bb32a0522770956a4bae15622ad91fcf6fc42bd7
Kachula Ivan <ka4ivan.dev@gmail.com>
2025-05-15 23:34:03 +0300

    add tests

--- tests/ViewSortableTest.php
+++ tests/ViewSortableTest.php
@@ -30,52 +30,61 @@ class ViewSortableTest extends TestCase
         $this->assertEquals($expected, \Sort::getSortLink('status', 'Status'));
     }
 
-//    public function test_facade_html_link_required_params_with_request()
-//    {
-//        request()->merge([
-//            'sort' => 'status',
-//            'order' => 'asc',
-//        ]);
-//
-//        $expected = '<a class="lte-sort-link" href="http://example.com?sort=status&order=desc" style="position: relative">Status </a>';
-//
-//        $this->assertEquals($expected, \Sort::getSortLink('status', 'Status'));
-//    }
+    public function test_facade_html_link_required_params_with_request()
+    {
+        request()->merge([
+            'sort' => 'status',
+            'order' => 'asc',
+        ]);
 
-//    public function test_facade_html_link_all_params()
-//    {
-//        $expected = '<a class="text-black" href="http://example.com?sort=city&order=desc&locale=uk" style="position: relative">City </a>';
-//
-//        $this->assertEquals($expected, \Sort::getSortLink(sort: 'city', text: 'City', order: 'desc', class: 'text-black', query: ['locale' => 'uk']));
-//    }
-//
-//    public function test_static_html_link_required_params()
-//    {
-//        $expected = '<a class="lte-sort-link" href="http://example.com?sort=phone&order=asc" style="position: relative">Phone </a>';
-//
-//        $this->assertEquals($expected, (new Sort)->getSortLink('phone', 'Phone'));
-//    }
-//
-//    public function test_facade_link_required_params()
-//    {
-//        $expected = 'http://example.com?sort=status&order=asc';
-//
-//        $this->assertEquals($expected, \Sort::getSortUrl('status', 'asc'));
-//    }
-//
-//    public function test_static_link_required_params()
-//    {
-//        $expected = 'http://example.com?sort=status&order=asc';
-//
-//        $this->assertEquals($expected, (new Sort)->getSortUrl('status', 'asc'));
-//    }
-//
-//    public function test_facade_link_all_params()
-//    {
-//        $expected = 'http://example.com?sort=name&order=asc&locale=uk';
-//
-//        $this->assertEquals($expected, \Sort::getSortUrl('name', 'asc', ['locale' => 'uk']));
-//    }
+        $expected = '<a class="lte-sort-link" href="http://example.com/posts?sort=status&order=desc" style="position: relative">Status </a>';
+
+        $this->assertEquals($expected, \Sort::getSortLink('status', 'Status'));
+    }
+
+    public function test_facade_html_link_all_params()
+    {
+        $expected = '<a class="text-black" href="http://example.com/posts?sort=city&order=desc&locale=uk" style="position: relative">City </a>';
+
+        $this->assertEquals($expected, \Sort::getSortLink(sort: 'city', text: 'City', order: 'desc', class: 'text-black', query: ['locale' => 'uk']));
+    }
+
+    public function test_static_html_link_required_params()
+    {
+        $expected = '<a class="lte-sort-link" href="http://example.com/posts?sort=phone&order=asc" style="position: relative">Phone </a>';
+
+        $this->assertEquals($expected, (new Sort)->getSortLink('phone', 'Phone'));
+    }
+
+    public function test_static_html_link_required_params_from_request()
+    {
+        $expected = '<a class="lte-sort-link" href="http://example.com/posts?sort=phone&order=desc" style="position: relative">Phone </a>';
+
+        Request::replace(['sort' => 'status', 'order' => 'asc']);
+
+        $this->assertEquals($expected, (new Sort)->getSortLink('phone', 'Phone'));
+    }
+
+    public function test_facade_link_required_params()
+    {
+        $expected = 'http://example.com/posts?sort=status&order=asc';
+
+        $this->assertEquals($expected, \Sort::getSortUrl('status', 'asc'));
+    }
+
+    public function test_static_link_required_params()
+    {
+        $expected = 'http://example.com/posts?sort=status&order=asc';
+
+        $this->assertEquals($expected, (new Sort)->getSortUrl('status', 'asc'));
+    }
+
+    public function test_facade_link_all_params()
+    {
+        $expected = 'http://example.com/posts?sort=name&order=asc&locale=uk';
+
+        $this->assertEquals($expected, \Sort::getSortUrl('name', 'asc', ['locale' => 'uk']));
+    }
 
     public function test_facade_next_order()
     {
@@ -87,9 +96,10 @@ class ViewSortableTest extends TestCase
         $this->assertEquals('desc', \Sort::getNextOrder('asc'));
     }
 
-//    public function test_facade_next_order_from_request()
-//    {
-//        Request::replace(['order' => 'desc']);
-//        $this->assertEquals('asc', \Sort::getNextOrder());
-//    }
+    public function test_facade_next_order_from_request()
+    {
+        Request::replace(['sort' => 'status', 'order' => 'desc']);
+
+        $this->assertEquals('asc', \Sort::getNextOrder());
+    }
 }
